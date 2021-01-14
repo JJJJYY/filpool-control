eab4a03848c4d603dd971a1b2d7ca9bd81ba41a5
DESKTOP-GFKCGBS\Administrator <459295355@qq.com>
2021-01-14 14:33:36 +0800

    3期

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -4,13 +4,13 @@ import { UploadOutlined } from '@ant-design/icons';
 import { connect } from 'umi';
 
 class UploadItem extends Component {
-  state = { fileList: [], previewImage: null }
+  state = { fileList: [], previewImage: null, showDefault: true };
 
   componentDidMount() {
     const { onRef, dispatch, tokenTime } = this.props;
     if (onRef) onRef(this);
     const time = new Date();
-    if (time.getTime() - tokenTime > 3000*1000) {
+    if (time.getTime() - tokenTime > 3000 * 1000) {
       dispatch({
         type: 'qiniu/token',
       });
@@ -19,65 +19,93 @@ class UploadItem extends Component {
 
   urls = () => {
     var urls = [];
-    this.state.fileList.map((file) => {
+    this.state.fileList.map(file => {
       if (file.status === 'done' && file.response) {
         urls.push(file.response.key);
       }
     });
     return urls.length ? urls[0] : null;
-  }
+  };
 
-  handleCancel = () => this.setState({ previewImage: null })
+  handleCancel = () => this.setState({ previewImage: null });
 
-  handlePreview = (file) => {
+  handlePreview = file => {
     this.setState({
       previewImage: file.url || file.thumbUrl,
     });
-  }
+  };
 
-  beforeUpload = (file) => {
-    const ispic = file.type === 'image/jpeg' || file.type === 'image/jpg' || file.type === 'image/png';
-    if (!ispic) {
-      message.error('请选择JPEG或PNG格式文件');
-    }
-    return ispic;
-  }
+  beforeUpload = file => {
+    return new Promise((resolve, reject) => {
+      const ispic =
+        file.type === 'image/jpeg' ||
+        file.type === 'image/jpg' ||
+        file.type === 'image/png';
+      if (!ispic) {
+        message.error('请选择JPEG或PNG格式文件');
+        return reject(false);
+      }
+      return resolve(true);
+    });
+  };
 
   render() {
-    const { fileList, previewImage } = this.state;
-    const { limit, token, onChange } = this.props;
+    const { fileList, previewImage, showDefault } = this.state;
+    const { limit, token, onChange, value } = this.props;
     const uploadButton = (
       <div>
         <UploadOutlined />
         <div>选择上传</div>
       </div>
     );
+
     return (
       <div>
         <Upload
-          name='file'
+          name="file"
           // accept='.png,.jpeg,.jpg'
-          action='https://up-z2.qiniup.com/'
-          listType='picture-card'
-          data={{ token : token }}
-          fileList={fileList}
-          onChange={(info) => {
-            const ispic = info.file.type === 'image/jpeg' || info.file.type === 'image/jpg' || info.file.type === 'image/png';
+          action="https://up-z2.qiniup.com/"
+          listType="picture-card"
+          data={{ token: token }}
+          fileList={fileList.length ? fileList : null}
+          maxCount={limit}
+          defaultFileList={
+            value && [
+              {
+                name: value,
+                url: value,
+              },
+            ]
+          }
+          onChange={info => {
+            const ispic =
+              info.file.type === 'image/jpeg' ||
+              info.file.type === 'image/jpg' ||
+              info.file.type === 'image/png';
             if (ispic) {
-              this.setState({ fileList: info.fileList })
+              this.setState({ fileList: info.fileList, showDefault });
               onChange && onChange(this.urls());
+            } else {
+              this.setState({ showDefault: false });
             }
           }}
           onPreview={this.handlePreview}
           beforeUpload={this.beforeUpload}
         >
-          {fileList.length >= (limit == null ? 1 : limit) ? null : uploadButton}
+          {fileList.length >= (limit == null ? 1 : limit) ||
+          (value && showDefault)
+            ? null
+            : uploadButton}
         </Upload>
-        <Modal visible={previewImage !== null} footer={null} onCancel={this.handleCancel}>
-          <img alt='' style={{ width: '100%' }} src={previewImage} />
+        <Modal
+          visible={previewImage !== null}
+          footer={null}
+          onCancel={this.handleCancel}
+        >
+          <img alt="" style={{ width: '100%' }} src={previewImage} />
         </Modal>
       </div>
-    )
+    );
   }
 }
 
