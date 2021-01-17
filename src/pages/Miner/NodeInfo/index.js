de629dfb75becf1c19bac8e18221e7b56ddbc046
DESKTOP-GFKCGBS\Administrator <459295355@qq.com>
2021-01-17 11:06:51 +0800

    1

--- src/pages/Miner/NodeInfo/index.js
+++ src/pages/Miner/NodeInfo/index.js
@@ -18,34 +18,56 @@ class Page extends Component {
     {
       title: '合作方',
       dataIndex: 'partner',
-    }, {
+    },
+    {
       title: '节点号',
       dataIndex: 'miner',
-    }, {
+    },
+    {
       title: '充币',
       dataIndex: 'top_up_coin_amount',
-    }, {
+    },
+    {
       title: '借币',
       dataIndex: 'lent_coin_amount',
-    }, {
+    },
+    {
       title: '有效算力',
       dataIndex: 'valid_power',
-    }, {
+    },
+    {
       title: '可用余额',
       dataIndex: 'available_balance',
-    }, {
+    },
+    {
       title: '质押余额',
       dataIndex: 'pledge_balance',
-    }, {
+    },
+    {
       title: '锁仓余额',
       dataIndex: 'lock_balance',
-    }, {
+    },
+    {
+      title: '预提交余额',
+      dataIndex: 'precommit_balance',
+    },
+    {
       title: '账户余额',
       dataIndex: 'account_balance',
-    }, {
+    },
+    {
       title: 'GAS',
       dataIndex: 'gas_fee',
-    }, {
+    },
+    {
+      title: '24H GAS',
+      dataIndex: 'gas_fee_24',
+    },
+    {
+      title: '24H 收益',
+      dataIndex: 'reward_24',
+    },
+    {
       title: '日期',
       dataIndex: 'time_node',
     },
@@ -63,7 +85,7 @@ class Page extends Component {
         page: page,
         count: count,
         search: search,
-      }
+      },
     });
   };
 
@@ -79,15 +101,15 @@ class Page extends Component {
           total={data ? data.total : 0}
           current={data ? data.current : 0}
           loading={listLoading}
-          onChange={(pagination) => {
+          onChange={pagination => {
             this.state.page = pagination.current;
             this.state.count = pagination.pageSize;
-            this.loadData()
+            this.loadData();
           }}
           rowKey="id"
         />
       </div>
-    )
+    );
   }
 }
 
